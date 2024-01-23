2962d3b7032bd51be64639b4b0497f99950607f4
KAVIYA-PRIYA <kaviyapriyak.21ece@kongu.edu>
2024-01-23 21:09:17 +0530

    fifth commit

--- promise.js
+++ promise.js
@@ -26,32 +26,88 @@ display('hey').then(function(status) {
         return display('hola');
     }
 });
-*/
+
 //promise
 function isLocationValid(loc) {
-    var v = "namakkal";
-    if (v == loc) {
-      return true;
-    } else {
-      return false;
-    }
+  var v = "namakkal";
+  if (v == loc) {
+    return true;
+  } else {
+    return false;
   }
-  function location(response, errorMessage) {
-    return new Promise(
-      setTimeOut(function (response,errorMsg) {
-        loc = "namakkal";
+}
+function location() {
+  return new Promise(
+    setTimeOut(function () {
+      loc = "namakkal";
+      console.log(loc);
+      if (isLocationValid(loc))
+        return response(loc, "Location found");
+      else
+        return errorMessage("Location not found");
+    }, 2000)
+  );
+}
+location()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (errorMessage) {
+    console.log(errorMessage);
+  });*/
+
+  function isLocationValid(loc) {
+    var validLocation = "namakkal";
+    return loc === validLocation;
+  }
+  
+  function location() {
+    return new Promise((resolve, reject)=>{
+      setTimeout(function () {
+        var loc = "namakkal";
         console.log(loc);
-        if (isLocationValid(loc)) return response(loc, "Location found");
-        else return errorMessage("Location not found");
-      }, 2000)
-    );
+        if (isLocationValid(loc)) {
+          resolve("Location found");
+        } else {
+          reject("Location not found");
+        }
+      }, 2000);
+    });
   }
+  
   location()
-    .then(function (response) {
+    .then((response) =>{
       console.log(response);
     })
-    .catch(function (errorMessage) {
+    .catch((errorMessage) =>{
       console.log(errorMessage);
     });
   
-  
+//Callback
+
+
+function database(){
+    console.log("Form submitted successfully..!!")
+ }
+ 
+ function submitForm(buttonClick,db){ // function definition
+    setTimeout(()=>{
+     console.log(buttonClick)
+         db()
+    },2000)
+ }
+ submitForm("Form submit button clicked",database) // Function calling
+
+ //Async await
+ async function asAw() {
+    var promise = new Promise(
+        function (response) {
+            setTimeout(function () {
+                //console.log(response)
+                response("Example for async/Await");
+            }, 1000)
+        }
+    );
+    console.log(await promise);
+}
+asAw()
